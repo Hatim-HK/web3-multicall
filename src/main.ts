21299eb78a95e36f97cd0ea93b1bd0923cbb1734
Hatim-HK <hatim.khomsi@singularitydao.ai>
2025-07-14 16:00:06 +0100

    feat: swagger interface

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Web3 Multicall API')
+    .setDescription('API for batching on-chain balance calls')
+    .setVersion('1.0')
+    .addTag('Onchain')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
   console.log('Application running on http://localhost:3000');
 }
